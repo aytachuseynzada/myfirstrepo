4de0faafbed16a20181286f03122f75a9a6759bf
aytachuseynzada <aytachuseynzade77@gmail.com>
2025-11-22 22:03:14 +0400

    inheritance

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        String text = "  Hello World  ";
+        System.out.println(text.toLowerCase());
+        System.out.println(text.toUpperCase());
+        System.out.println(text.replace("l", "z"));
+        System.out.println(text.contains("orld"));
+        System.out.println(text.charAt(3));
+        System.out.println(text.substring(3, 8));
+        System.out.println(text.trim());
+        System.out.println(text.startsWith("Hell"));
+
+        public static void
+    }
+}
